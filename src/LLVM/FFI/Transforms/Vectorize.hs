f0cc7ec23de85a550801cd8aeb09fc38ba7e34a8
llvm <llvm@henning-thielemann.de>
2016-08-15 13:48:21 +0000

    Transforms.Vectorize

---
+++ src/LLVM/FFI/Transforms/Vectorize.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE Safe #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+module LLVM.FFI.Transforms.Vectorize where
+
+import LLVM.FFI.Core (PassManagerRef)
+
+
+foreign import ccall unsafe "LLVMAddBBVectorizePass" addBBPass
+    :: PassManagerRef -> IO ()
+
+foreign import ccall unsafe "LLVMAddLoopVectorizePass" addLoopPass
+    :: PassManagerRef -> IO ()
+
+foreign import ccall unsafe "LLVMAddSLPVectorizePass" addSLPPass
+    :: PassManagerRef -> IO ()
