fb54f330f935db0006d9a70256083ce613ff0bd5
llvm <llvm@henning-thielemann.de>
2020-03-14 09:17:32 +0000

    example/Host: show host data layout

--- example/Host.hs
+++ example/Host.hs
@@ -3,11 +3,17 @@ This program shows some interesting facts about the host machine.
 -}
 module Main where
 
-import Common (getString)
+import Common (getString, createExecutionEngine)
 
+import qualified LLVM.FFI.ExecutionEngine as EE
+import qualified LLVM.FFI.Target as Target
 import qualified LLVM.FFI.Support.Host as Host
 import qualified LLVM.FFI.Core as LLVM
 import qualified LLVM.Target.Native as Native
+
+import Foreign.C.String (withCString)
+
+import Control.Exception (bracket)
 
 
 main :: IO ()
@@ -16,3 +22,10 @@ main = do
 
    putStrLn =<< getString Host.getHostCPUName
    putStrLn LLVM.hostTriple
+
+   modul <- withCString "host" LLVM.moduleCreateWithName
+   withCString LLVM.hostTriple $ LLVM.setTarget modul
+   bracket (createExecutionEngine modul) EE.disposeExecutionEngine $
+         \execEngine -> do
+      td <- EE.getExecutionEngineTargetData execEngine
+      putStrLn =<< getString (Target.copyStringRepOfTargetData td)
